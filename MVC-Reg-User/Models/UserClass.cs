f788c4c630cd413d6efa62213f17ad7713de89a2
Sivabala <sivabala2701@gmail.com>
2025-04-24 21:47:44 +0530

    Add project files.

---
+++ MVC-Reg-User/Models/UserClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Reg_User.Models
+{
+    public class UserClass
+    {
+        [Required(ErrorMessage = "Enter Username !")]
+        [Display(Name = "Enter UserName :")]
+        [StringLength(maximumLength:7,MinimumLength = 3,ErrorMessage = "Username Length Must Be Max 7 & Min 3")]
+        public string Uname { get; set; }
+        public string Uemail { get; set; }
+        public string Upwd { get; set; }
+        public string Repwd { get; set; }
+        public char Gender { get; set; }
+        public string Uimg { get; set; }
+    }
+}
---
+++ MVC-Reg-User/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Reg_User.Startup))]
+namespace MVC_Reg_User
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
