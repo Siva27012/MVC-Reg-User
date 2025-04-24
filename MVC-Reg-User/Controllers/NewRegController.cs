d1fd1bc661011a1e905d4c1924185a2c34f2ce05
Sivabala <sivabala2701@gmail.com>
2025-04-24 22:19:25 +0530

    Controller and class for User added

---
+++ MVC-Reg-User/Controllers/NewRegController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Reg_User.Models;
+
+namespace MVC_Reg_User.Controllers
+{
+    public class NewRegController : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserClass uc, HttpPostedFileBase file)
+        {
+            string maincon = ConfigurationManager.ConnectionStrings["Myconnec tion"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(maincon);
+            string sqlquery = "insert into [dbo].[MVCregUser] (Uname,Uemail,Upwd,Gender,Uimg) values (@Unmae,@email,@Upwd,@Gender,@Uimg)";
+            SqlCommand cmd = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            cmd.Parameters.AddWithValue("@Uname", uc.Uname);
+            cmd.Parameters.AddWithValue("@Uemail", uc.Uemail);
+            cmd.Parameters.AddWithValue("@Upwd", uc.Upwd);
+            cmd.Parameters.AddWithValue("@Gender", uc.Gender);
+            if (file != null) 
+            {
+                string filename = Path.GetFileName(file.FileName);
+                string imagePath = Path.Combine(Server.MapPath("~/User-Images/"), filename);
+                file.SaveAs(imagePath);
+            }
+            cmd.Parameters.AddWithValue("@Uimg","~/User-Images/"+file.FileName );
+            return View();
+        }
+    }
+}
--- MVC-Reg-User/Models/UserClass.cs
+++ MVC-Reg-User/Models/UserClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace MVC_Reg_User.Models
 {
@@ -12,10 +13,28 @@ namespace MVC_Reg_User.Models
         [Display(Name = "Enter UserName :")]
         [StringLength(maximumLength:7,MinimumLength = 3,ErrorMessage = "Username Length Must Be Max 7 & Min 3")]
         public string Uname { get; set; }
+
+        [Required(ErrorMessage = "Please Enter The Email Address !")]
+        [Display(Name = "Email Id :")]
         public string Uemail { get; set; }
+
+        [Required(ErrorMessage = "Enter The Password !")]
+        [Display(Name = "Password :")]
+        [DataType(DataType.Password)]
         public string Upwd { get; set; }
+
+        [Required(ErrorMessage = "Enter The RePassword !")]
+        [Display(Name = "Re-Password :")]
+        [DataType(DataType.Password)]
+        [Compare("Upwd")]
         public string Repwd { get; set; }
+
+        [Required(ErrorMessage = "Select The Gender !")]
+        [Display(Name = "Gender :")]
         public char Gender { get; set; }
+
+        [Required(ErrorMessage = "Upload Profile Image !")]
+        [Display(Name = "Profile Image :")]
         public string Uimg { get; set; }
     }
 }
