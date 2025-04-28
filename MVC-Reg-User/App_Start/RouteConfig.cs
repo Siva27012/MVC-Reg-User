a73101d32e091bd9c286924528ec7f786b0fd660
Sivabala <sivabala2701@gmail.com>
2025-04-28 21:40:25 +0530

    FIlePath and ViewData added

--- MVC-Reg-User/App_Start/RouteConfig.cs
+++ MVC-Reg-User/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC_Reg_User
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "NewReg", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- MVC-Reg-User/Controllers/NewRegController.cs
+++ MVC-Reg-User/Controllers/NewRegController.cs
@@ -21,22 +21,36 @@ namespace MVC_Reg_User.Controllers
         [HttpPost]
         public ActionResult Index(UserClass uc, HttpPostedFileBase file)
         {
-            string maincon = ConfigurationManager.ConnectionStrings["Myconnec tion"].ConnectionString;
+            string maincon = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
             SqlConnection sqlconn = new SqlConnection(maincon);
-            string sqlquery = "insert into [dbo].[MVCregUser] (Uname,Uemail,Upwd,Gender,Uimg) values (@Unmae,@email,@Upwd,@Gender,@Uimg)";
+            string sqlquery = "insert into [dbo].[MVCregUser] (Uname,Uemail,Upwd,Gender,Uimg) values (@Uname,@Uemail,@Upwd,@Gender,@Uimg)";
             SqlCommand cmd = new SqlCommand(sqlquery, sqlconn);
             sqlconn.Open();
             cmd.Parameters.AddWithValue("@Uname", uc.Uname);
             cmd.Parameters.AddWithValue("@Uemail", uc.Uemail);
             cmd.Parameters.AddWithValue("@Upwd", uc.Upwd);
             cmd.Parameters.AddWithValue("@Gender", uc.Gender);
+            string imagePath = null;
+
             if (file != null) 
             {
                 string filename = Path.GetFileName(file.FileName);
-                string imagePath = Path.Combine(Server.MapPath("~/User-Images/"), filename);
-                file.SaveAs(imagePath);
+                imagePath = "/User-Images/" + filename;
+                string fullPath = Server.MapPath(imagePath);
+
+                // Ensure the directory exists
+                string dir = Path.GetDirectoryName(fullPath);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                file.SaveAs(fullPath);
             }
             cmd.Parameters.AddWithValue("@Uimg","~/User-Images/"+file.FileName );
+            cmd.ExecuteNonQuery();
+            sqlconn.Close();
+            ViewData["Message"] = "User Record " + uc.Uname + " Is Saved Successfully !";
             return View();
         }
     }
